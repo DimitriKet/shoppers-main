0785b47e943807cbb2670df14bb9362fdb9cada6
Kwan <60667449+DimitriKet@users.noreply.github.com>
2025-01-25 12:48:55 +0700

    Update product.php

--- shoppers-admin/product.php
+++ shoppers-admin/product.php
@@ -1,137 +1,217 @@
+
 <?php
-  global $conn;
-  if(isset($_GET['id']))
-  {
-    $id = $_GET['id'];
-  }
-?>
-    
-    <div class="site-section">
-      <div class="container">
+function add() 
+{
+    global $conn;
+    if(isset($_POST['sbm']))
+    {
+      $img=upload('img');
+      $name=$_POST['name'];
+      $description=$_POST['description'];
+      $content=$_POST['content'];
+      $sql="
+      INSERT INTO `product` ( `name`, `img`,  `description`, `content`) 
+      VALUES  ('{$name}','{$img}','{$description}','{$content}')";
+        if(mysqli_query($conn, $sql))
+            message('Thành Công!','product');
+        else
+            message('Thất bại!','product');
+    }
+    else
+    {
+      echo '
+      <div style="padding:1%">
+      <form action="" method="POST" enctype="multipart/form-data">
+      <p>
+          <label for="name">Name</label>
+          <input class="form-control" name="name" type="text" value="" />
+      </p>
+      <p>
+          <label for="name">Image</label>
+          <input class="form-control" name="img"  type="file" />
+      </p>
+     
+      <p>
+          <label for="description">Description</label>
+          <textarea class="form-control" name="description"></textarea>
+      </p>
+      <p>
+      <label for="description">Content</label>
+          <textarea class="form-control" name="content" id="editor"></textarea>
+      </p>
+      <p>
+          <button type="submit"class="btn btn-primary" name="sbm">Thực hiện</button>
+      </p>
+      </div>
+      ';
+    }
+}
+
+function edit() 
+{
+    global $conn;
+    if(isset($_GET['id']))
+        $id = $_GET['id'];
+    else
+        $id = 0;
+    if(isset($_POST['sbm']))
+    {
+      $img=upload('img');
+      $name=$_POST['name'];
+      $description=$_POST['description'];
+      $content=$_POST['content'];
+      if($img!='')
+          $sql="
+          update product set 
+          `name`='{$name}',
+          `img`='{$img}',
+          `description`='{$description}',
+          `content`='{$content}'
+          where id=$id";
+      else
+          $sql="
+          update product set 
+          `name`='{$name}',
+          `description`='{$description}',
+          `content`='{$content}'
+          where id=$id";
+        if(mysqli_query($conn, $sql))
+            message('Thành Công!','product');
+        else
+            message('Thất bại!','product');
+    }
+    else
+    {
+        $sql = "select * from product where id = $id";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+        echo '
+      <div style="padding:1%">
+      <form action="" method="POST" enctype="multipart/form-data">
+      <p>
+          <label for="name">Name</label>
+          <input class="form-control" name="name" type="text" value="'.$row['name'].'" />
+      </p>
+      <p>
+          <label for="name">Image</label>
+          <input class="form-control" name="img"  type="file" />
+      </p>
+     
+      <p>
+          <label for="description">Description</label>
+          <textarea class="form-control" name="description">'.$row['description'].'</textarea>
+      </p>
+      <p>
+      <label for="description">Content</label>
+          <textarea class="form-control" name="content" id="editor">'.$row['content'].'</textarea>
+      </p>
+      <p>
+          <button type="submit"class="btn btn-primary" name="sbm">Thực hiện</button>
+      </p>
+      </div>
+      ';
+    }
+}
+
+function manage()
+{
+    global $conn;
+    $data=
+    "<table class='table' style='color:#7971ea;'>
+    <tr class='text-weight'>
+        <td>STT</td>
+        <td>Image</td>
+        <td>Name</td>
+        <td>Date</td>
+        <td>Edit</td>
+        <td>Delete</td>
+        <td>Status</td>
+    </tr>";
+    $stt=1;
+    $sql='select * from product';
+    $result=mysqli_query($conn,$sql); 
+    while($row=mysqli_fetch_assoc($result))
+    {
+        if($row['status']==1)
+        $status="<a href='index.php?cmd=product&action=status&id=".$row['id']."&status=".$row['status']."' class='btn btn-success'>Lock</a>";
+        else
+        $status="<a href='index.php?cmd=product&action=status&id=".$row['id']."&status=".$row['status']."' class='btn btn-secondary'>Unlock</a>";
+        $data.= "
+        <tr><td>".$stt++."</td>
+            <td><img src='../images/".$row['img']."' width='30'></td>
+            <td>".$row['name']."</td>
           
-        <div class="row mb-5">
+            <td>".$row['date']."</td>
+            <td><a href='index.php?cmd=product&action=edit&id=".$row['id']."' class='btn btn-warning'>Edit</a></td>
+            <td><a href='index.php?cmd=product&action=del&id=".$row['id']."' class='btn btn-danger'>Delete</a></td>
+            <td>".$status."</td>
+        </tr>";
+    }
+    $data.= '</table>';
+    echo $data;
+}
 
-          <div class="col-md-3 order-1 mb-5 mb-md-0">
-              <div class="border p-4 rounded mb-4">
-                <h3 class="mb-3 h6 text-uppercase text-black d-block">Categories</h3>
-                <ul class="list-unstyled mb-0">
-                  <li class="mb-1"><a href="#" class="d-flex"><span>Men</span> <span class="text-black ml-auto">(2,220)</span></a></li>
-                  <li class="mb-1"><a href="#" class="d-flex"><span>Women</span> <span class="text-black ml-auto">(2,550)</span></a></li>
-                  <li class="mb-1"><a href="#" class="d-flex"><span>Children</span> <span class="text-black ml-auto">(2,124)</span></a></li>
-                </ul>
-              </div>
+function del() 
+{
+    global $conn;
+    if(isset($_GET['id']))
+        $id = $_GET['id'];
+    else
+        $id = 0;
 
-              <div class="border p-4 rounded mb-4">
-                <div class="mb-4">
-                  <h3 class="mb-3 h6 text-uppercase text-black d-block">Filter by Price</h3>
-                  <div id="slider-range" class="border-primary"></div>
-                  <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white" disabled="" />
+    $sql = "delete from product where id = $id";
+    if (mysqli_query($conn, $sql)) 
+        message("Success", "product");
+    else 
+        message("Failed", "product");
+}
+
+function status() 
+{
+    global $conn;
+
+    if(isset($_GET['id']))
+        $id = $_GET['id'];
+    else
+        $id = 0;
+    if ($_GET['status'] == 1)   
+        $sql = "update product set status = 0 where id = $id";
+    else
+        $sql = "update product set status = 1 where id = $id";
+    if (mysqli_query($conn, $sql)) 
+        message("Success", "product");
+    else 
+        message("Failed", "product");
+}
+?>
+        
+<section>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12">
+                <div class="d-flex justify-content-center gap-20 py-20">
+                    <a href="index.php?cmd=product&action=add" class="btn btn-sm btn-primary">Add new product</a>
+                    <a href="index.php?cmd=product-category" class="btn btn-sm btn-secondary">View product</a>
+                    <a href="index.php?cmd=product&action=manage" class="btn btn-sm btn-success">Manage product</a>
                 </div>
-
-                <div class="mb-4">
-                  <h3 class="mb-3 h6 text-uppercase text-black d-block">Size</h3>
-                  <label for="s_sm" class="d-flex">
-                    <input type="checkbox" id="s_sm" class="mr-2 mt-1"> <span class="text-black">Small (2,319)</span>
-                  </label>
-                  <label for="s_md" class="d-flex">
-                    <input type="checkbox" id="s_md" class="mr-2 mt-1"> <span class="text-black">Medium (1,282)</span>
-                  </label>
-                  <label for="s_lg" class="d-flex">
-                    <input type="checkbox" id="s_lg" class="mr-2 mt-1"> <span class="text-black">Large (1,392)</span>
-                  </label>
-                </div>
-
-                <div class="mb-4">
-                  <h3 class="mb-3 h6 text-uppercase text-black d-block">Color</h3>
-                  <a href="#" class="d-flex color-item align-items-center" >
-                    <span class="bg-danger color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Red (2,429)</span>
-                  </a>
-                  <a href="#" class="d-flex color-item align-items-center" >
-                    <span class="bg-success color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Green (2,298)</span>
-                  </a>
-                  <a href="#" class="d-flex color-item align-items-center" >
-                    <span class="bg-info color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Blue (1,075)</span>
-                  </a>
-                  <a href="#" class="d-flex color-item align-items-center" >
-                    <span class="bg-primary color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Purple (1,075)</span>
-                  </a>
-                </div>
-
-              </div>
             </div>
-
-            <div class="col-md-9 order-2">
-
-              <div class="row">
-                <div class="col-md-12 mb-5">
-                  <div class="float-md-left mb-4"><h2 class="text-black h5">See All</h2></div>
-                  <div class="d-flex">
-                    <div class="dropdown mr-1 ml-md-auto">
-                      <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Latest
-                      </button>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                        <a class="dropdown-item" href="#">Men</a>
-                        <a class="dropdown-item" href="#">Women</a>
-                        <a class="dropdown-item" href="#">Children</a>
-                      </div>
-                    </div>
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" id="dropdownMenuReference" data-toggle="dropdown">Reference</button>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuReference">
-                        <a class="dropdown-item" href="#">Relevance</a>
-                        <a class="dropdown-item" href="#">Name, A to Z</a>
-                        <a class="dropdown-item" href="#">Name, Z to A</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Price, low to high</a>
-                        <a class="dropdown-item" href="#">Price, high to low</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row mb-5">
-                
-                <?php
-                $data = '';
-                  $sql = "select * from product where status = 1 and idcategory = {$id}";
-                  $result = mysqli_query($conn, $sql);
-                  while ($row=mysqli_fetch_assoc($result))
-                  {
-                    $data .= '
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                      <div class="block-4 text-center border">
-                        <figure class="block-4-image">
-                          <a href="index.php?cmd=product-detail&id='. $row['id'].'"><img src="../images/'.$row['img'].'" alt="Image placeholder" class="img-fluid"></a>
-                        </figure>
-                        <div class="block-4-text p-4">
-                          <h3><a href="shop-single.html">'.$row['name'].'</a></h3>
-                          <p class="text-secondaty font-weight-bold">'. number_format($row['price']).' VND</p>
-                        </div>
-                      </div>
-                    </div>';
-                  }
-                  echo $data;
-                ?>
-              </div>
-              <div class="row" data-aos="fade-up">
-                <div class="col-md-12 text-center">
-                  <div class="site-block-27">
-                    <ul>
-                      <li><a href="#">&lt;</a></li>
-                      <li class="active"><span>1</span></li>
-                      <li><a href="#">2</a></li>
-                      <li><a href="#">3</a></li>
-                      <li><a href="#">4</a></li>
-                      <li><a href="#">5</a></li>
-                      <li><a href="#">&gt;</a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+            <div class="col-12">
+            <?php
+            if(isset($_GET['action']))
+                $action=$_GET['action'];
+            else 
+                $action = 'manage';
+            
+            switch($action)
+            {
+                case 'edit': edit(); break;
+                case 'status': status(); break;
+                case 'add': add(); break;
+                case 'del': del(); break;
+                default: manage();
+            }
+            ?>
             </div>
-
-          </div>
-
         </div>
     </div>
+</section>
